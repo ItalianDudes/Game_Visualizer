039976ff300e2c5c7cc92537b36de6fb082f3496
Hacker6329 <Hacker6329@gmail.com>
2022-03-29 00:18:39 +0200

    Created DirectoryHandler. Need to be implemented

---
+++ src/main/java/com/italianDudes/game_visualizer/server/classes/DirectoryHandler.java
@@ -0,0 +1,8 @@
+package com.italianDudes.game_visualizer.server.classes;
+
+public final class DirectoryHandler {
+
+    private DirectoryHandler(){
+        throw new UnsupportedOperationException("Can't instantiate this class!");
+    }
+}
