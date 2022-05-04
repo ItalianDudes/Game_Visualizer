8c687820aec3d94cdc405d1d4bc55601663d81e5
Hacker6329 <Hacker6329@gmail.com>
2022-05-04 02:15:35 +0200

    Implemented shadow plugin for create fatJar. Changed build.gradle for create the fatJar. jar now is called shadowJar

--- src/main/java/com/italianDudes/game_visualizer/server/Server.java
+++ src/main/java/com/italianDudes/game_visualizer/server/Server.java
@@ -4,9 +4,17 @@
  */
 package com.italianDudes.game_visualizer.server;
 
+import java.io.File;
+import java.io.IOException;
+
 public final class Server {
 
     public static void start() {
         System.out.println("Hello World! I'm a server.");
+
+        String str = "Paolo";
+        String strHash = org.apache.commons.codec.digest.DigestUtils.sha256Hex(str);
+
+        System.out.println(strHash);
     }
 }
